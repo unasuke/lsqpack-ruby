63f991121538591dbc414ddd80e109ec9627d9ea
Yusuke Nakamura <yusuke1994525@gmail.com>
2023-03-19 12:29:18 +0900

    Build ls-qpack in extconf

--- ext/lsqpack/extconf.rb
+++ ext/lsqpack/extconf.rb
@@ -2,4 +2,16 @@
 
 require "mkmf"
 
+repo_root = File.join(File.expand_path(__dir__), "../", "../")
+
+$CPPFLAGS = "-I #{repo_root}"
+$LDFLAGS = "-lls-qpack -L#{File.join(repo_root, "ls-qpack")}"
+
+Dir.chdir(repo_root) do
+  Dir.chdir("ls-qpack") do
+    `cmake .`
+    `make`
+  end
+end
+
 create_makefile("lsqpack/lsqpack")
